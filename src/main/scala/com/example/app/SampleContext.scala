08210162cb9d1fb0c06578a859270d36b92ea611
Jeff Garratt <garratt.jeff@gmail.com>
2019-07-25 18:27:27 -0400

    Moving to use SampleContext.
    Signed-off-by: Jeff Garratt <garratt.jeff@gmail.com>

--- src/main/scala/com/example/app/SampleContext.scala
+++ src/main/scala/com/example/app/SampleContext.scala
@@ -1,16 +1,58 @@
 package com.example.app
 
-import com.github.jeffgarratt.hl.fabric.sdk.{Context, Directory, Network, Port}
+import com.github.jeffgarratt.hl.fabric.sdk._
 import com.github.jeffgarratt.hl.fabric.sdk.Directory.Cert
+import com.google.protobuf.ByteString
+import com.typesafe.config.ConfigFactory
+import main.app.AppDescriptors
+import org.hyperledger.fabric.protos.peer.chaincode.ChaincodeSpec
 
-class SampleContext extends Context {
-  override def getDirectory: Option[Directory] = None
+import scala.collection.concurrent.TrieMap
+import scala.util.Random
 
-  val network = new Network
+class SampleContext(override val projectName: String, override val rootPath: Seq[String]) extends LocalDockerContext(projectName = projectName, rootPath = rootPath) {
 
-  override def getNetwork: Network = network
 
-  override def getPortHostMapping(serviceName: String, port: Int): Either[String, Port] = Left("Not Implemented")
+  val dev0Org0 = getDirectory.get.users.find(_.name == "dev0Org0").get
+  val nodeAdminTuple = Directory.natsForUser(getDirectory.get, dev0Org0)(0)
+  val cchExample02 = new ChaincodeHelper(this, dev0Org0, nodeAdminTuple, endorsers = List("peer0"))
+  // partial func helpers for building chaincode specs and invocation specs
+  val defaultChannelName = "com.peerorg0.blockchain.channel.medical"
+  val getChaincodeSpec = Endorser.getChaincodeSpec(chaincodeType = ChaincodeSpec.Type.GOLANG, path = "github.com/hyperledger/fabric/examples/chaincode/go/marketplace/app_mgr", name = "appmgr", _: List[ByteString], version = "1.0")
+  val getInvocationSpec = Endorser.InvocationSpec(_: ChaincodeSpec, channelName = Some(defaultChannelName), proposalResponseHandler = Some(Endorser.getHandler(AppDescriptors)))
 
-  override def getTrustedRootsForPeerNetworkAsPEM: List[Cert] = List[Cert]()
+  val deliverSpecOrderer0 = Deliver.DeliverSpec(dev0Org0, nodeAdminTuple, defaultChannelName, "orderer0", port = 7050, seekInfo = Deliver.seekInfoAllAndWait, timeout = 10 minutes)
+  val deliverSpecPeer0 = Deliver.DeliverSpec(dev0Org0, nodeAdminTuple, defaultChannelName, "peer0", port = 7051, seekInfo = Deliver.seekInfoAllAndWait, timeout = 10 minutes)
+  val broadcastSpecOrderer0 = Orderer.BroadcastSpec(nodeName = "orderer0", timeout = 10 minutes)
+
+  def getMedicalChannelForOrg(medicalOrg: Organization) = {
+    s"com.${medicalOrg.name.toLowerCase()}.blockchain.channel.medical"
+  }
+
+}
+
+
+object SampleContext {
+
+  val conf = ConfigFactory.load()
+
+  private val contextMap = TrieMap.empty[String, Context]
+
+  def getContext(projectName: String) = {
+    contextMap.getOrElseUpdate(projectName, {
+      val fabricPrototypePath = conf.getString("fabric.prototype.path")
+      new SampleContext(projectName = projectName, rootPath = fabricPrototypePath.split("/") ++ Seq("tmp"))
+    })
+  }
+
+  val rand = Random
+
+  def getRandInt(min: Int, max: Int) = {
+    var num = -1
+    while (num < min) {
+      num = rand.nextInt(max)
+    }
+    num
+  }
+
 }
