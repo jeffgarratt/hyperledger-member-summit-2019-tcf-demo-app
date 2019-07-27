e624340ac2f3954a7563882e6543590b356c7c73
Jeff Garratt <garratt.jeff@gmail.com>
2019-07-27 09:35:11 -0400

    Added separate Network impl for GRPC conn override
    Demonstrate how to override the management of grpc
    channels.
    
    Signed-off-by: Jeff Garratt <garratt.jeff@gmail.com>

--- src/main/scala/com/example/app/SampleContext.scala
+++ src/main/scala/com/example/app/SampleContext.scala
@@ -1,13 +1,40 @@
 package com.example.app
 
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+
+import com.github.jeffgarratt.hl.fabric.sdk.Directory.Cert
 import com.github.jeffgarratt.hl.fabric.sdk._
 import com.typesafe.config.ConfigFactory
+import io.grpc.ManagedChannel
+import io.grpc.internal.DnsNameResolverProvider
 
 import scala.collection.concurrent.TrieMap
 import scala.util.Random
 
+class MyNetwork extends Network {
+
+  override def getGRPCChannel(ipAddress: String, port: Int, root_certificates: List[Cert], ssl_target_name_override: String): ManagedChannel = {
+    // Open a channel and get the AdminGrpc stub and check status
+    import io.grpc.netty.{GrpcSslContexts, NettyChannelBuilder}
+    val certsAsPEMInputStream = new ByteArrayInputStream(root_certificates.mkString("\n").getBytes(StandardCharsets.UTF_8))
+    val channel = NettyChannelBuilder.forAddress(ipAddress, port).nameResolverFactory(new DnsNameResolverProvider()).overrideAuthority(ssl_target_name_override).sslContext(GrpcSslContexts.forClient.trustManager(certsAsPEMInputStream).build).build
+    scribe.debug("inside MyNetwork!!!!!")
+    channel
+  }
+}
+
+
 class SampleContext(override val projectName: String, override val rootPath: Seq[String]) extends LocalDockerContext(projectName = projectName, rootPath = rootPath) {
 
+  val myNetwork = new MyNetwork()
+
+  override def getNetwork: Network = {
+    SampleContext.conf.getString("app.context.network.selected") match{
+      case (s : String) if (s == SampleContext.conf.getString("app.context.network.option.myNetwork")) => myNetwork
+      case _ => network
+    }
+  }
 }
 
 
